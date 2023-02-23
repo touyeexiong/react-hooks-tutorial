39a201b0ba12acdd1c6366a02e90fe65eac1fe29
Tou Xiong <touyeexiong@gmail.com>
2023-02-22 21:48:53 -0600

    at 20:00 of tutorial.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import { useForm } from "./useForm";
 
 function App() {
-  const [values, handleChange] = useForm({email: '', password: ''})
+  const [values, handleChange] = useForm({email: '', password: '', firstName: '' })
+
+  useEffect(() => {
+    console.log('render');
+// cleanup function below
+    return() => {
+      console.log('unmount');
+      
+    }
+
+  }, []); 
+  // dependency array
 
   return (
   <div>
     <input 
       name="email" 
       value={values.email}
-      onChange={handleChange}/>
+      onChange={handleChange}
+      placeholder="email"
+      />
+    <input
+      name="firstName"
+      placeholder="first name"
+      value={values.firstName}
+      onChange={handleChange}
+      />  
     <input 
     type="password" 
     name="password"
+    placeholder="password"
     value={values.password} 
     onChange={handleChange}
     />
