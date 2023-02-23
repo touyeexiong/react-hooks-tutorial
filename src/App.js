07b1afa4055ddb5ba7785fd439fcdecaacf34bba
Tou Xiong <touyeexiong@gmail.com>
2023-02-22 21:38:40 -0600

    learned how to use useState and why it's effective. Going to learn useEffect next.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
-
+import React, {useState} from "react";
+import { useForm } from "./useForm";
 
 function App() {
+  const [values, handleChange] = useForm({email: '', password: ''})
 
-
-  return <div>Hey</div>
-}
+  return (
+  <div>
+    <input 
+      name="email" 
+      value={values.email}
+      onChange={handleChange}/>
+    <input 
+    type="password" 
+    name="password"
+    value={values.password} 
+    onChange={handleChange}
+    />
+  </div>
+  );
+};
 
 export default App;
